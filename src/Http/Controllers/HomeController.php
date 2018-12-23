91d608cb2072ba6beaf1a818110302d81198f61d
Jon Cowie <jonlives@gmail.com>
2018-12-23 13:54:28 +0000

    Fix fax method
    Signed-off-by: Jon Cowie <jonlives@gmail.com>

--- src/Http/Controllers/HomeController.php
+++ src/Http/Controllers/HomeController.php
@@ -57,7 +57,7 @@ class HomeController extends Controller
                 $user->has_fax = $fax_list[1] > 0;
                 $fax_names = array();
                 foreach($fax_list[0] as $fax) {
-                    array_push($fax_names, $super['type']['typeName']);
+                    array_push($fax_names, $fax['type']['typeName']);
                 }
 
                 $user->fax_names = implode(', ', $fax_names);
